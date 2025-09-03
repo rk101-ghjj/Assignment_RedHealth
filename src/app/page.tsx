0896b5b5ee5a08432166ed057149f68df9bef3d9
red-kamalesh <123550828+red-kamalesh@users.noreply.github.com>
2025-09-03 09:00:10 +0530

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <div className="flex justify-center items-center h-screen">
+        <h1 className="text-3xl ">Red Playground</h1>
+      </div>
+    </div>
+  );
+}
